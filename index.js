0e7c2537bb23274b6e6a6fa814f115e504add578
Cat Tuong <cattuong510@gmail.com>
2024-04-25 18:30:38 +0700

    add video call api and fix router

--- index.js
+++ index.js
@@ -1,105 +1,127 @@
-const express = require('express');
-const {firebase,db}= require('./config')
-const cron = require('node-cron')
+const express = require("express");
+const { firebase, db } = require("./config");
+const cron = require("node-cron");
 const PORT = 3000;
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-const { getFirestore, collection, getDocs,addDoc, updateDoc, doc, setDoc,getDoc} = require('firebase/firestore');
+const {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  setDoc,
+  getDoc,
+} = require("firebase/firestore");
 const firestore = getFirestore(firebase);
-const { sendNotification} = require('./controllers/User')
-const {get1PHistory} = require('./controllers/Question')
+const { sendNotification } = require("./controllers/User");
+const { get1PHistory } = require("./controllers/Question");
 
-const SendNoti= async()=>{
-    const myCollection = collection(firestore, 'Users');
-    const querySnapshot = await getDocs(myCollection);
-    const list = querySnapshot.docs.map((doc) => {
-        const data = doc.data();
-        const docId = doc.id;
-        return { ...data, Id: docId };
+const SendNoti = async () => {
+  const myCollection = collection(firestore, "Users");
+  const querySnapshot = await getDocs(myCollection);
+  const list = querySnapshot.docs.map((doc) => {
+    const data = doc.data();
+    const docId = doc.id;
+    return { ...data, Id: docId };
+  });
+  list.forEach((user) => {
+    if (user.vocabAlarms) {
+      user.vocabAlarms.forEach((item) => {
+        let timeParts = item.Time.split(/:| /);
+        if (timeParts.length === 4) {
+          if (timeParts[3] === "PM") {
+            timeParts[0] = String(parseInt(timeParts[0]) + 12);
+          }
+        } else {
+          timeParts[0] = String(parseInt(timeParts[0]));
+        }
+
+        let h = parseInt(timeParts[0]);
+        let m = parseInt(timeParts[1]);
+        let s = parseInt(timeParts[2]);
+        let x = s + " " + m + " " + h + " * * *";
+        cron.schedule(x, () => {
+          sendNotification(user.token, item.Id);
+        });
       });
-    list.forEach((user) => {
-        if(user.vocabAlarms){
-        user.vocabAlarms.forEach((item)=>{
-            let timeParts = item.Time.split(/:| /);
-            if (timeParts.length === 4) {
-                if (timeParts[3] === "PM") {
-                  timeParts[0] = String(parseInt(timeParts[0]) + 12);
-                }
-              } else {
-                  timeParts[0] = String(parseInt(timeParts[0]));
-              }
-              
-              let h = parseInt(timeParts[0]);
-              let m = parseInt(timeParts[1]);
-              let s = parseInt(timeParts[2]);
-              let x = s + " " + m + " "+ h + " * * *"
-            cron.schedule(x, () => {
-                sendNotification(user.token,item.Id);
-              });
-        })
     }
-      });
-}
-const cors = require('cors')
+  });
+};
+const cors = require("cors");
 const app = express();
-const router = require('./router/router1')
+const router = require("./router/router1");
 
-const http = require('http');
-const server = http.createServer(app)
-const {Server}= require("socket.io")
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server);
-const moment = require('moment');
-let userId = '6uz50o2mYWORgoBVzmYsHZYyq622';
-io.on('connection', (socket) => {
-//   console.log('New Client connected');
-  socket.on('UserId',(data)=>{
+const moment = require("moment");
+let userId = "6uz50o2mYWORgoBVzmYsHZYyq622";
+
+io.use((socket, next) => {
+  if (socket.handshake.query) {
+    let callerId = socket.handshake.query.callerId;
+    socket.user = callerId;
+    next();
+  }
+});
+
+io.on("connection", (socket) => {
+  socket.on("UserId", (data) => {
     userId = data;
-    console.log(data)
-  })
+    console.log(data);
+  });
   //realtime cho alarmVocab
-  db.collection('Users').doc(userId)
-  .onSnapshot((doc)=>{
-    if (doc.exists) {
-      const list = doc.data().vocabAlarms;
-      const list1 = doc.data().SavedQuestion;
-      // const name = userId+'Alarmchange'
-      // io.emit(name, list)
-      io.emit(userId+'savedQ', list1)
-    } else {
-      // const name = userId+'Alarmchange'
-      // io.emit(name, [])
-    }
-  })
-    //realtime cho history home
-    db.collection('Users').doc(userId)
-    .onSnapshot((doc)=>{
+  db.collection("Users")
+    .doc(userId)
+    .onSnapshot((doc) => {
+      if (doc.exists) {
+        const list = doc.data().vocabAlarms;
+        const list1 = doc.data().SavedQuestion;
+        // const name = userId+'Alarmchange'
+        // io.emit(name, list)
+        io.emit(userId + "savedQ", list1);
+      } else {
+        // const name = userId+'Alarmchange'
+        // io.emit(name, [])
+      }
+    });
+  //realtime cho history home
+  db.collection("Users")
+    .doc(userId)
+    .onSnapshot((doc) => {
       if (doc.exists) {
         const list = doc.data().HistoryPractice;
-        if(list){
-            const name = userId+'PHistorychange'
-            get1PHistory(list).then(item => {io.emit(name, item)});
+        if (list) {
+          const name = userId + "PHistorychange";
+          get1PHistory(list).then((item) => {
+            io.emit(name, item);
+          });
         }
-      } 
-    })
-    //realtime cho Practice Plan
-    db.collection('PracticePlan').doc(userId)
-    .onSnapshot((doc)=>{
+      }
+    });
+  //realtime cho Practice Plan
+  db.collection("PracticePlan")
+    .doc(userId)
+    .onSnapshot((doc) => {
       if (doc.exists) {
         const list = doc.data();
-        if(list){
-            const name = userId+'PracticePlanChange'
-            
-            io.emit(name, list);
+        if (list) {
+          const name = userId + "PracticePlanChange";
+
+          io.emit(name, list);
         }
-      } 
-    })
-    //realtime cho TestHistory
-    db.collection('TestHistory')
+      }
+    });
+  //realtime cho TestHistory
+  db.collection("TestHistory")
     //.where('UserId', '==', userId)
     .onSnapshot((querySnapshot) => {
       const dataList = [];
-      
+
       querySnapshot.forEach((doc) => {
         const data = doc.data();
         if (data) {
@@ -107,106 +129,114 @@ io.on('connection', (socket) => {
           dataList.push(data);
         }
       });
-  
+
       if (dataList.length > 0) {
-        const name = userId + 'TestHistoryChange';
+        const name = userId + "TestHistoryChange";
         io.emit(name, dataList);
       }
     });
-     //realtime cho posts in forum
-  db.collection('Posts')
-  .orderBy('postTime.seconds', 'desc')
-  .onSnapshot((querySnapshot)=>{
-    const list = [];
-    const list2 = [];
-    querySnapshot.forEach(doc =>{
-      const dateSeconds = moment.unix(doc.data().postTime.seconds);
-      const data = {...doc.data(),postTime:dateSeconds.format('DD-MM-YYYY HH:mm')}
-      if(data.Allow == true){
-        list.push(data)
-      }
-      if(doc.data().userId == userId){
-        list2.push(data)
-      }
-      //realtime cho list comment trong post
-      io.emit(doc.data().postId,doc.data().comments)
-      //realtime cho detailpost
-      io.emit(doc.data().postId+'detailpost',data)
-    })
-    io.emit('mainPosts', list)
-    //realtime cho posts in profile
-    const name = userId+'userPosts'
-    io.emit(name, list2)
-  })
-    //realtime cho 1 comments in posts
-    db.collection('Comments')
-    .onSnapshot((querySnapshot)=>{
-      querySnapshot.forEach(doc =>{
-        io.emit(doc.data().commentId, doc.data())
-      })
-    })
-    //realtime cho notification
-    db.collection('Notification')
-    .orderBy('time', 'desc')
-    .onSnapshot((querySnapshot)=>{
-      querySnapshot.forEach(doc =>{
-        if(doc.data().classify=='reply'){
-          io.emit(doc.data().CommentownerId+'noti', doc.data())
-          if(doc.data().Read=='no'){
-            io.emit(doc.data().CommentownerId+'sign', '1')
+  //realtime cho posts in forum
+  db.collection("Posts")
+    .orderBy("postTime.seconds", "desc")
+    .onSnapshot((querySnapshot) => {
+      const list = [];
+      const list2 = [];
+      querySnapshot.forEach((doc) => {
+        const dateSeconds = moment.unix(doc.data().postTime.seconds);
+        const data = {
+          ...doc.data(),
+          postTime: dateSeconds.format("DD-MM-YYYY HH:mm"),
+        };
+        if (data.Allow == true) {
+          list.push(data);
+        }
+        if (doc.data().userId == userId) {
+          list2.push(data);
+        }
+        //realtime cho list comment trong post
+        io.emit(doc.data().postId, doc.data().comments);
+        //realtime cho detailpost
+        io.emit(doc.data().postId + "detailpost", data);
+      });
+      io.emit("mainPosts", list);
+      //realtime cho posts in profile
+      const name = userId + "userPosts";
+      io.emit(name, list2);
+    });
+  //realtime cho 1 comments in posts
+  db.collection("Comments").onSnapshot((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      io.emit(doc.data().commentId, doc.data());
+    });
+  });
+  //realtime cho notification
+  db.collection("Notification")
+    .orderBy("time", "desc")
+    .onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        if (doc.data().classify == "reply") {
+          io.emit(doc.data().CommentownerId + "noti", doc.data());
+          if (doc.data().Read == "no") {
+            io.emit(doc.data().CommentownerId + "sign", "1");
+          }
+        } else {
+          io.emit(doc.data().PostownerId + "noti", doc.data());
+          if (doc.data().Read == "no") {
+            io.emit(doc.data().PostownerId + "sign", "1");
           }
         }
-        else{
-          io.emit(doc.data().PostownerId+'noti', doc.data())
-          if(doc.data().Read=='no'){
-            io.emit(doc.data().PostownerId+'sign', '1')
-          }
-        }
-      })
-    })
+      });
+    });
+
+  socket.on("chat message", ({ roomId, message }) => {
+    io.to(roomId).emit("chat message", { message });
+  });
+
   // Xử lý khi client gửi yêu cầu
-  socket.on('connect', (data) => {
-    console.log("socket connected")
+  socket.on("connect", (data) => {
+    console.log("socket connected");
   });
 });
 
 // app.use(express.json());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
-app.use(cors())
-app.use('/api',router)
-app.post('/upload', upload.single('image'), (req, res) => {
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
+app.use(cors());
+app.use("/api", router);
+app.post("/upload", upload.single("image"), (req, res) => {
   // Do something with the uploaded image
   const file = req.file;
   console.log(file);
-  
-  res.json({ message: 'Image uploaded successfully',photo:file.path });
+
+  res.json({ message: "Image uploaded successfully", photo: file.path });
 });
-app.post('/uploadaudio', upload.single('audio'), (req, res) => {
+app.post("/uploadaudio", upload.single("audio"), (req, res) => {
   // Do something with the uploaded image
   const file = req.file;
   console.log(file);
-  
-  res.json({ message: 'Audio uploaded successfully',audio:file.path });
+
+  res.json({ message: "Audio uploaded successfully", audio: file.path });
 });
-app.post('/uploadvideo', upload.single('video'), (req, res) => {
+app.post("/uploadvideo", upload.single("video"), (req, res) => {
   // Do something with the uploaded image
   const file = req.file;
-  console.log(file);   
-  res.json({ message: 'Image uploaded successfully',video:file.path });
+  console.log(file);
+  res.json({ message: "Image uploaded successfully", video: file.path });
 });
-app.post('/uploadpdf', upload.single('pdf'), (req, res) => {
+app.post("/uploadpdf", upload.single("pdf"), (req, res) => {
   // Do something with the uploaded image
   const file = req.file;
-  console.log(file);   
-  res.json({ message: 'Image uploaded successfully',filepdf:file.path });
+  console.log(file);
+  res.json({ message: "Image uploaded successfully", filepdf: file.path });
 });
 
 // server.listen(PORT, () => {
 //     console.log(`Server is running on http://localhost:${PORT}`);
 // })
-if (process.env.NODE_ENV !== 'test') {
-  server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+if (process.env.NODE_ENV !== "test") {
+  server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
 
 // app.on("close", () => {
--- router/router1.js
+++ router/router1.js
@@ -1,71 +1,147 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const {getVocabLesson, getVocabinLesson, getVocabs, setAlarmVocab, getAlarmVocab, updateAlarmVocab, addVocab, addVocabLesson, updateTopic, deleteTopic,updateVocab,deleteVocab} = require('../controllers/Vocab')
-const {setUserInfo, updateUser, getAllUsers, getUserData,updateUserPrivate} = require ('../controllers/User')
-const {uploadAudio, uploadPracticeHistory} = require ('../controllers/SWskills')
-const {getQuestion, pushPracticeHistory, getOneQuestion, addQuestion, countQuestion, getAllQuestion, updateQuestion, deleteQuestion, getReviewQuestion, getSavedQuestion, updateSavedQ} = require('../controllers/Question')
-const {uploadPracticePlan, getPracticePlan, updatePracticePlan} = require('../controllers/PracticePlan')
-const {addPost, updatePost, addComment, getOneComment, addNotification, deleteNotification, updateNotification,
-     filterOnlyPost,filterOnlyhashtag,filterBoth, pushSavedPost, getSavedPost, deletePost, getPosts}=require('../controllers/Post')
-const {addTest, getAllTest, updateTest, deleteTest, uploadTestHistory} = require('../controllers/Test')
+const {
+  getVocabLesson,
+  getVocabinLesson,
+  getVocabs,
+  setAlarmVocab,
+  getAlarmVocab,
+  updateAlarmVocab,
+  addVocab,
+  addVocabLesson,
+  updateTopic,
+  deleteTopic,
+  updateVocab,
+  deleteVocab,
+} = require("../controllers/Vocab");
+const {
+  setUserInfo,
+  updateUser,
+  getAllUsers,
+  getUserData,
+  updateUserPrivate,
+} = require("../controllers/User");
+const {
+  uploadAudio,
+  uploadPracticeHistory,
+} = require("../controllers/SWskills");
+const {
+  getQuestion,
+  pushPracticeHistory,
+  getOneQuestion,
+  addQuestion,
+  countQuestion,
+  getAllQuestion,
+  updateQuestion,
+  deleteQuestion,
+  getReviewQuestion,
+  getSavedQuestion,
+  updateSavedQ,
+} = require("../controllers/Question");
+const {
+  uploadPracticePlan,
+  getPracticePlan,
+  updatePracticePlan,
+} = require("../controllers/PracticePlan");
+const {
+  addPost,
+  updatePost,
+  addComment,
+  getOneComment,
+  addNotification,
+  deleteNotification,
+  updateNotification,
+  filterOnlyPost,
+  filterOnlyhashtag,
+  filterBoth,
+  pushSavedPost,
+  getSavedPost,
+  deletePost,
+  getPosts,
+} = require("../controllers/Post");
+const {
+  addTest,
+  getAllTest,
+  updateTest,
+  deleteTest,
+  uploadTestHistory,
+} = require("../controllers/Test");
+
+const {
+  initiateCall,
+  updateCall,
+  addChatRoom,
+  getChatRoom,
+  getUserChatRooms,
+  updateChatRoom,
+  deleteChatRoom,
+} = require("../controllers/Chat");
 
 //vocab
-router.get('/VocabLessons',getVocabLesson)
-router.get('/VocabinLesson/:TopicId',getVocabinLesson)
-router.get('/Vocabs',getVocabs)
-router.get('/getAlarmVocab/:userId',getAlarmVocab)
-router.post('/alarmVocab/:userId',setAlarmVocab)
-router.put('/updateAlarmVocab/:userId', updateAlarmVocab)
+router.get("/VocabLessons", getVocabLesson);
+router.get("/VocabinLesson/:TopicId", getVocabinLesson);
+router.get("/Vocabs", getVocabs);
+router.get("/getAlarmVocab/:userId", getAlarmVocab);
+router.post("/alarmVocab/:userId", setAlarmVocab);
+router.put("/updateAlarmVocab/:userId", updateAlarmVocab);
 //user
-router.put('/setUserInfo/:userId', setUserInfo)
-router.put('/updateUser/:userId', updateUser)
-router.put('/updateUserPrivate/:userId', updateUserPrivate)
-router.get('/Users', getAllUsers)
-router.get('/UserData/:userId', getUserData)
+router.put("/setUserInfo/:userId", setUserInfo);
+router.put("/updateUser/:userId", updateUser);
+router.put("/updateUserPrivate/:userId", updateUserPrivate);
+router.get("/Users", getAllUsers);
+router.get("/UserData/:userId", getUserData);
 //question
-router.get('/Question/:Part/:userId/:number',getQuestion)
-router.post('/PracticeHistory/:userId/:sign',pushPracticeHistory)
-router.get('/oneQuestion/:Part/:Qid',getOneQuestion)
-router.post('/Question/:Part/add',addQuestion)
-router.get('/countQuestion/:Part',countQuestion)
-router.get('/getAllQuestion/:Part',getAllQuestion)
-router.put('/updateQuestion/:Part/:Qid', updateQuestion)
-router.delete('/deleteQuestion/:Part/:Qid',deleteQuestion)
-router.post('/ReviewQuestion',getReviewQuestion)
-router.get('/getSavedQuestion/:userId',getSavedQuestion)
-router.put('/updateSavedQuestion/:userId', updateSavedQ)
-router.post('/uploadAudio', uploadAudio)
-router.post('/uploadPracticeHistory', uploadPracticeHistory)
+router.get("/Question/:Part/:userId/:number", getQuestion);
+router.post("/PracticeHistory/:userId/:sign", pushPracticeHistory);
+router.get("/oneQuestion/:Part/:Qid", getOneQuestion);
+router.post("/Question/:Part/add", addQuestion);
+router.get("/countQuestion/:Part", countQuestion);
+router.get("/getAllQuestion/:Part", getAllQuestion);
+router.put("/updateQuestion/:Part/:Qid", updateQuestion);
+router.delete("/deleteQuestion/:Part/:Qid", deleteQuestion);
+router.post("/ReviewQuestion", getReviewQuestion);
+router.get("/getSavedQuestion/:userId", getSavedQuestion);
+router.put("/updateSavedQuestion/:userId", updateSavedQ);
+router.post("/uploadAudio", uploadAudio);
+router.post("/uploadPracticeHistory", uploadPracticeHistory);
 //PracticePlan
-router.post('/PracticePlan/:userId/add', uploadPracticePlan)
-router.get('/PracticePlan/:userId', getPracticePlan)
-router.put('/PracticePlan/:userId/update', updatePracticePlan)
+router.post("/PracticePlan/:userId/add", uploadPracticePlan);
+router.get("/PracticePlan/:userId", getPracticePlan);
+router.put("/PracticePlan/:userId/update", updatePracticePlan);
 //post
-router.post('/addPost', addPost)
-router.put('/updatePost/:postId', updatePost)
-router.post('/addComment/:sign/:momId', addComment)
-router.post('/addNotification', addNotification)
-router.get('/getoneComment/:commentId',getOneComment)
-router.delete('/deleteNoti/:notiId',deleteNotification)
-router.put('/updateNoti/:notiId', updateNotification)
-router.get('/filterOnlyhashtag/:hashtag',filterOnlyhashtag)
-router.get('/filterOnlyPost/:userId/:type',filterOnlyPost)
-router.get('/filterBoth/:userId/:type/:hashtag',filterBoth)
-router.put('/savePost/:userId/:postId', pushSavedPost)
-router.get('/getsavePost/:userId',getSavedPost)
-router.delete('/deletePost/:postId',deletePost)
-router.get('/getPosts',getPosts)
+router.post("/addPost", addPost);
+router.put("/updatePost/:postId", updatePost);
+router.post("/addComment/:sign/:momId", addComment);
+router.post("/addNotification", addNotification);
+router.get("/getoneComment/:commentId", getOneComment);
+router.delete("/deleteNoti/:notiId", deleteNotification);
+router.put("/updateNoti/:notiId", updateNotification);
+router.get("/filterOnlyhashtag/:hashtag", filterOnlyhashtag);
+router.get("/filterOnlyPost/:userId/:type", filterOnlyPost);
+router.get("/filterBoth/:userId/:type/:hashtag", filterBoth);
+router.put("/savePost/:userId/:postId", pushSavedPost);
+router.get("/getsavePost/:userId", getSavedPost);
+router.delete("/deletePost/:postId", deletePost);
+router.get("/getPosts", getPosts);
 //addvocab
-router.post('/addVocabLesson', addVocabLesson)
-router.post('/addVocab', addVocab)
-router.delete('/deleteTopic/:topicId', deleteTopic)
-router.delete('/deleteVocab/:vocabId', deleteVocab)
-router.put('/updateVocab/:vocabId', updateVocab)
-router.put('/updateTopic/:topicId', updateTopic)
+router.post("/addVocabLesson", addVocabLesson);
+router.post("/addVocab", addVocab);
+router.delete("/deleteTopic/:topicId", deleteTopic);
+router.delete("/deleteVocab/:vocabId", deleteVocab);
+router.put("/updateVocab/:vocabId", updateVocab);
+router.put("/updateTopic/:topicId", updateTopic);
 //test
-router.post('/Test/add', addTest)
-router.get('/Tests', getAllTest)
-router.put('/Test/update/:testId', updateTest)
-router.delete('/Test/delete/:testId', deleteTest)
-router.post('/Test/TestHistory/:userId/upload', uploadTestHistory)
-module.exports = router
+router.post("/Test/add", addTest);
+router.get("/Tests", getAllTest);
+router.put("/Test/update/:testId", updateTest);
+router.delete("/Test/delete/:testId", deleteTest);
+router.post("/Test/TestHistory/:userId/upload", uploadTestHistory);
+//chat
+router.post("/Chat/initiateCall", initiateCall);
+router.post("/Chat/updateCall", updateCall);
+router.post("/ChatRoom/add", addChatRoom);
+router.get("/ChatRoom/:id", getChatRoom);
+router.get("/ChatRoom/getUserChatRoom/:userId", getUserChatRooms);
+router.put("/ChatRoom/update/:id", updateTest);
+router.delete("/ChatRoom/delete/:id", deleteTest);
+module.exports = router;
